37799ad4fa5590163d75176d2e40bc90956765a2
Suteja Indrayana <indrayanasoetedja@gmail.com>
2021-05-02 01:30:58 +0700

    fix responsive, added obit homepage widget

--- theme/js/main.js
+++ theme/js/main.js
@@ -46,4 +46,60 @@ jQuery(document).ready(function () {
 			},
 		],
 	});
+
+	//Testimonial slider widget js
+	$("#testimonial-slider").slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: false,
+		// centerPadding: true,
+		dots: false,
+		dotsClass: "slick-dots",
+		prevArrow:
+			"<button type='button' class='slick-prev pull-left'><img src='./theme/assets/prev-icon.png'></button>",
+		nextArrow:
+			"<button type='button' class='slick-next pull-right'><img src='./theme/assets/next-icon.png'></button>",
+		responsive: [
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+				},
+			},
+		],
+	});
+
+	// Homepage obit slider
+	$("#obit-slides").slick({
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		autoplay: false,
+		// centerPadding: true,
+		dots: false,
+		dotsClass: "slick-dots",
+		prevArrow:
+			"<button type='button' class='slick-prev pull-left'><img src='./theme/assets/prev-arrow-rounded.png'></button>",
+		nextArrow:
+			"<button type='button' class='slick-next pull-right'><img src='./theme/assets/next-arrow-rounded.png'></button>",
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					arrows: false,
+				},
+			},
+		],
+	});
 });
